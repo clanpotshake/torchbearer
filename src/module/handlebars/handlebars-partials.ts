35a2c206fa3f11b7ba81f0a7cf5f2b91954d5f0a
clanpotshake <>
2021-11-03 17:32:22 -0400

    wip

--- src/module/handlebars/handlebars-partials.ts
+++ src/module/handlebars/handlebars-partials.ts
@@ -2,8 +2,14 @@ export default async function registerHandlebarsPartials(): Promise<void> {
   // register every component and tab used in the main sheet classes
   const templatePaths = [
     'systems/torchbearer/templates/sheets/actor/components/bio-data.hbs',
+    'systems/torchbearer/templates/sheets/actor/components/bigs.hbs',
+    'systems/torchbearer/templates/sheets/actor/components/rewards.hbs',
+    'systems/torchbearer/templates/sheets/actor/components/traits.hbs',
+    'systems/torchbearer/templates/sheets/actor/components/trait.hbs',
     'systems/torchbearer/templates/sheets/actor/components/skill.hbs',
     'systems/torchbearer/templates/sheets/actor/components/skills.hbs',
+    'systems/torchbearer/templates/sheets/actor/components/abilities.hbs',
+    'systems/torchbearer/templates/sheets/actor/components/wises.hbs',
     'systems/torchbearer/templates/sheets/item/components/body.hbs',
     'systems/torchbearer/templates/sheets/item/components/sheet-header.hbs',
     'systems/torchbearer/templates/sheets/item/tabs/description.hbs',
