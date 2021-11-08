de3027bc628cadeb366924345e0a2bba5fbbb2f1
clanpotshake <>
2021-11-08 09:28:05 -0500

    wises tab works now

--- src/module/rolls/TBRoll.ts
+++ src/module/rolls/TBRoll.ts
@@ -1,7 +1,34 @@
+import { getGame } from '../helpers';
+
 export class TBRoll<D extends Record<string, unknown> = Record<string, unknown>> extends Roll<D> {
   static CHAT_TEMPLATE = 'systems/torchbearer/templates/dice/dice/roll.hbs';
 
+  /**
+   * @override
+   */
   async render(chatOptions: Parameters<Roll['render']>[0] = {}): Promise<string> {
-    return super.render(chatOptions);
+    logger.info('TBRoll.render', this);
+    chatOptions = foundry.utils.mergeObject(
+      {
+        user: getGame().user?.id,
+        flavor: null,
+        template: TBRoll.CHAT_TEMPLATE,
+        blind: false,
+      },
+      chatOptions,
+    );
+    const isPrivate = chatOptions.isPrivate;
+
+    // Execute the roll, if needed
+    if (!this._evaluated) this.evaluate();
+
+    const chatData = {
+      formula: isPrivate ? '???' : this._formula,
+      flavor: isPrivate ? null : chatOptions.flavor,
+      user: chatOptions.user,
+      tooltip: isPrivate ? '' : await this.getTooltip(),
+      total: isPrivate ? '?' : Math.round((this.total ?? 0) * 100) / 100,
+    };
+    return renderTemplate(chatOptions.template ?? '', chatData);
   }
 }
--- src/module/rolls/TBTest.ts
+++ src/module/rolls/TBTest.ts
@@ -1,22 +1,38 @@
 export class TBTest extends DiceTerm {
   constructor({ modifiers = [], results = [], options }: Partial<DiceTerm.TermData>) {
-    logger.info('roll constructor', results);
     super({
       faces: 6,
       results,
       modifiers,
       options,
     });
-    // if (this.results.length > 0) {
-    //   this.evaluateResults();
-    // }
+    logger.info('TBTest.constructor', results);
+    if (this.results.length > 0) {
+      this.evaluateResults();
+    }
   }
+  rerollableSixes = 0;
+  rerollableFails = 0;
 
   /** @override */
   roll({ minimize = false, maximize = false } = {}): DiceTerm.Result {
-    logger.info('roll', this);
-    // Swap minimize / maximize because in DS4, the best possible roll is a 1 and the worst possible roll is a 20
-    const x = super.roll({ minimize: maximize, maximize: minimize });
+    logger.info('TBTest.roll', this);
+    const x = super.roll({ minimize: minimize, maximize: maximize });
     return x;
+  }
+  evaluateResults(): void {
+    this.rerollableFails = 0;
+    this.rerollableSixes = 0;
+    this.results.map((die) => {
+      if (die.result == 6) {
+        this.rerollableSixes++;
+      }
+      // TODO mastery here
+      if (die.result <= 4) {
+        this.rerollableFails++;
+      }
+    });
+    logger.info('found sixes:', this.rerollableSixes);
+    logger.info('found fails:', this.rerollableFails);
   }
 }
