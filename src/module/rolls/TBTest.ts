cea81a911583eefc9629643c24c1d4444356b05f
clanpotshake <>
2022-01-17 09:02:29 -0500

    wip i guess

--- src/module/rolls/TBTest.ts
+++ src/module/rolls/TBTest.ts
@@ -1,32 +1,34 @@
+import { getGame } from '../helpers';
+
 export class TBTest extends DiceTerm {
-  constructor({ modifiers = [], results = [], options }: Partial<DiceTerm.TermData>) {
+  constructor({ number = 1, modifiers = [], results = [], options }: Partial<DiceTerm.TermData>) {
     super({
+      number: number,
       faces: 6,
       results,
       modifiers,
       options,
     });
-    logger.info('TBTest.constructor', results);
+    logger.info('TBTest.constructor', this);
+    // 5dsv4 = 5 dice, ob4
+    // 6dsvs3 = 6 dice opposed roll against 3
+    const checkTargetNumberModifier = this.modifiers.filter((m) => m[0] === 'v')[0];
+    const ctnRgx = new RegExp('vs?([0-9]+)?');
+    const ctnMatch = checkTargetNumberModifier?.match(ctnRgx);
+
     if (this.results.length > 0) {
       this.evaluateResults();
-    } else {
-      logger.error('TBTest has empty results', this);
     }
   }
   rerollableSixes = 0;
   rerollableFails = 0;
   successes = 0;
+  ob = 0;
+  opposedRoll = false;
 
   /** @override */
-  // evaluate(
-  //   options: Partial<RollTerm.EvaluationOptions & { async: boolean }> | undefined = {},
-  // ): Promise<this> {
-  //   logger.info('in TBTest.evaluate');
-  //   return super.evaluate({ minimize: false, maximize: true, async: options?.async || true });
-  // }
-  /** @override */
   get expression(): string {
-    return `ds${this.modifiers.join('')}`;
+    return `${this.number}ds${this.modifiers.join('')}`;
   }
   /** @override */
   get total(): string | number | null | undefined {
@@ -36,30 +38,19 @@ export class TBTest extends DiceTerm {
   /** @override */
   roll({ minimize = false, maximize = false } = {}): DiceTerm.Result {
     logger.info('in TBTest.roll');
-    if (!this._evaluated) {
-      logger.info('roll not yet evaluated, doing so...');
-      super.roll({ minimize: false, maximize: maximize });
-      this.evaluateResults();
-    }
+    super.roll({ minimize: false, maximize: maximize });
+    this.evaluateResults();
     return {
       result: this.successes,
       success: true,
     };
   }
   evaluateResults(): void {
-    this.rerollableFails = 0;
-    this.rerollableSixes = 0;
-    this.results.map((die) => {
-      if (die.result == 6) {
-        this.rerollableSixes++;
-      }
-      // TODO mastery here
-      if (die.result < 4) {
-        this.rerollableFails++;
-      } else {
-        this.successes++;
-      }
-    });
+    const res = evaluateTest(
+      this.results.map((die) => die.result),
+      this.ob,
+    );
+    this.results = res;
     logger.info('found sixes:', this.rerollableSixes);
     logger.info('found fails:', this.rerollableFails);
     logger.info('found successes:', this.successes);
@@ -85,4 +76,33 @@ export class TBTest extends DiceTerm {
     v: (): void => undefined, // Modifier is consumed in constructor for checkTargetNumber
     n: (): void => undefined, // Modifier is consumed in constructor for canFumble
   };
+}
+
+export function evaluateTest(dice: number[], ob: number): SubCheckResult[] {
+  if (dice.length < 1) {
+    throw new Error(getGame().i18n.localize('TB2.ErrorInvalidNumberOfDice'));
+  }
+  let passes = 0;
+  let sixes = 0;
+  let fails = 0;
+  dice.map((die) => {
+    if (die == 6) sixes++;
+    if (die >= 4) {
+      passes++;
+    } else {
+      fails++;
+    }
+  });
+  const result = {
+    sixes: sixes,
+    fails: fails,
+    result: passes,
+    success: passes >= ob,
+  };
+  return [result];
+}
+
+interface SubCheckResult extends DiceTerm.Result {
+  // TODO do i need anything in here?
+  foo?: number;
 }
